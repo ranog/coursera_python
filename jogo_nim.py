930e76a681f067579f87843714b7610de2cdc6b0
ranog <jprnogueira@yahoo.com.br>
2017-08-02 22:03:11 -0300

    Iniciando a implementação do jogo_nim.py

---
+++ jogo_nim.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def computador_escolhe_jogada(n, m):
+    return jogada
+
+def  usuario_escolhe_jogada(n, m):
+    return jogada
+
+def partida():
+
+    print("Bem-vindo ao jogo do NIM! Escolha:")
+    print("")
+    print("1 - para jogar uma partida isolara")
+    print("2 - para jogar um campeonato")
+    print("")
+
+
+    n = input(int("Número de peças inicial: "))
+    m = input(int("Número máximo de peças para retirar: "))
+
+
