fa326b600528106d388eaad30b7fb333d3ba4457
ranog <jprnogueira@yahoo.com.br>
2020-03-26 14:07:41 -0300

    Tentando iniciar os estudos novamentecat .gitignore !

---
+++ maior_elemento.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Exercício 1 - Maior elemento de uma lista
+Escreva a função maior_elemento que recebe como parâmetro uma lista
+com números inteirose devolve um número inteiro correspondente ao
+maior valor presente na lista recebida.
+"""
+
+def maior_elemento(numeros):
+    maximo = max(numeros)
+    return(maximo)
+
+
